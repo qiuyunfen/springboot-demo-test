4cb30809871358906e6e21b8069c9f31d354c8de
qiuyunfen <1150382430@qq.com>
2017-04-11 17:47:38 +0800

    add Order model

--- src/main/java/com/springboot/model/Order.java
+++ src/main/java/com/springboot/model/Order.java
@@ -1,5 +1,37 @@
 package com.springboot.model;
 
-
 public class Order {
+    private CartItemPrice cartItemPrice;
+    private double sum;
+    private double save;
+
+    public CartItemPrice getCartItemPrice() {
+        return cartItemPrice;
+    }
+
+    public void setCartItemPrice(CartItemPrice cartItemPrice) {
+        this.cartItemPrice = cartItemPrice;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public double getSave() {
+        return save;
+    }
+
+    public void setSave(double save) {
+        this.save = save;
+    }
+
+    public Order(CartItemPrice cartItemPrice, double sum, double save) {
+        this.cartItemPrice = cartItemPrice;
+        this.sum = sum;
+        this.save = save;
+    }
 }
